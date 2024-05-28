67a19198151844cf31534f94981d619892519207
Darkhan Zhursin <daratechn@gmail.com>
2024-05-27 22:33:35 -0500

    basic mvn structure

---
+++ examples/lesson01/lesson01-basic-spring-application/src/main/java/basic/CustomerService.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class CustomerService {
+
+    public CustomerService() {
+        super();
+        System.out.println("Inside the constructor");
+    }
+
+    public void sayHello() {
+        System.out.println("Hello from CustomerService");
+    }
+}
