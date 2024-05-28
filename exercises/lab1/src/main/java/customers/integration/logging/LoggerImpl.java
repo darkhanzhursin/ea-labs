73c80447ae87656b7e1ed64d6b79bbd5986b06e3
Darkhan Zhursin <daratechn@gmail.com>
2024-05-28 16:36:48 -0500

    configured using Java configuration + classpath scanning + autowiring

--- exercises/lab1/src/main/java/customers/integration/logging/LoggerImpl.java
+++ exercises/lab1/src/main/java/customers/integration/logging/LoggerImpl.java
@@ -1,7 +1,10 @@
 package customers.integration.logging;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 
+@Service
 public class LoggerImpl implements Logger{
 
 	public void log(String logstring) {
