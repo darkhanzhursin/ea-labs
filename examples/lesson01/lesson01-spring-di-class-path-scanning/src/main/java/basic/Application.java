047f639598d97d400f5e19fe3516f0e071098b06
Darkhan Zhursin <daratechn@gmail.com>
2024-05-28 08:15:17 -0500

    path scanning

---
+++ examples/lesson01/lesson01-spring-di-class-path-scanning/src/main/java/basic/Application.java
@@ -0,0 +1,14 @@
+package basic;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new
+                ClassPathXmlApplicationContext("springconfig.xml");
+        CustomerService customerService = context.getBean("customerService", CustomerService.class);
+        customerService.addCustomer();
+    }
+}
---
+++ examples/lesson01/lesson01-spring-di-class-path-scanning/src/main/java/basic/EmailService.java
@@ -0,0 +1,10 @@
+package basic;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    public void sendEmail() {
+        System.out.println("Sending email");
+    }
+}
