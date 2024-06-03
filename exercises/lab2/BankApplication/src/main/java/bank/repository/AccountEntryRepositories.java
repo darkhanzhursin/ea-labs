0924196decd86546875835adaddb54ab27b5d3bf
Darkhan Zhursin <daratechn@gmail.com>
2024-06-02 19:01:49 -0500

    deleted redundance DAOs

--- exercises/lab2/BankApplication/src/main/java/bank/repository/AccountEntryRepositories.java
+++
@@ -1,7 +0,0 @@
-package bank.repository;
-
-import bank.domain.AccountEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountEntryRepositories extends JpaRepository<AccountEntry, Long> {
-}
